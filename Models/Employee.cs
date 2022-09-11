1da0a7c7e3f7c1bfea5542d55c34b415e9c63552
cjones1047 <cjmj1047@gmail.com>
2022-09-11 11:05:22 -0500

    Employee.cs created, models for Department and Employee added and referenced as Departments and Employees tables respectively, build successful

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,6 +1,33 @@
-﻿namespace HrDbProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HrDbProject.Models
 {
+    [Table("Departments")]
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? DeptName { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+
+    }
+
+    [Table("Employees")]
     public class Employee
     {
+        [Key]
+
+        public int Id { get; set; }
+        [ForeignKey("Department")]
+        public int FKDeptId { get; set; }  
+        public string? EmpName { get; set; }
+        public decimal Salary { get; set; }
+        public bool IsActive { get; set; }
+        public Department? Department { get; set; }
     }
 }
